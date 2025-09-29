762705bbdea426730566f19173215c412cbbf427
Rasmustofth <rasmustofthermansen@gmail.com>
2025-09-30 00:28:14 +0200

    Fix a bug with my settings button that happend when a user hit the rate limit

--- ReciptHelper/app/routes/Settings.tsx
+++ ReciptHelper/app/routes/Settings.tsx
@@ -20,7 +20,6 @@ async function GetSettingsValues()
   const data = await response.json();
   let sletAutoKvit = data["sletAutomatiskKvitteringer"];
   let visKvit = data["showOldKvitteringer"]
-
   setAutomatiksSletning(sletAutoKvit)
   setVisGamleKvitteringer(visKvit)
 }
@@ -33,34 +32,43 @@ useEffect(()=>{
 
 
 async function skiftAutomatiskSletning() {
+  automatiskSletning? setAutomatiksSletning(false) : setAutomatiksSletning(true);
   setFormData({
       value : automatiskSletning
       });
-    automatiskSletning? setAutomatiksSletning(false) : setAutomatiksSletning(true);
-    const response = await SetSettings(formData);
+    
+    await SetSettings(formData);
   }
   async function skiftVisGamleKvitteringer() {
+    visGamleKvitteringer? setVisGamleKvitteringer(false) : setVisGamleKvitteringer(true);
       setVisGamleKvitteringerformData({
     value : visGamleKvitteringer
     });
-    visGamleKvitteringer? setVisGamleKvitteringer(false) : setVisGamleKvitteringer(true);
-    const response = await SetSettingsForOldRecipts(visGamleKvitteringerformData);
+    await SetSettingsForOldRecipts(visGamleKvitteringerformData);
   }
-
+  
   return (
     <ProtectedRoute>
       <div className="w-full h-full flex flex-col justify-center items-center">
         <div className="grid md:grid-cols-3 grid-cols-1 gap-14 px-11">
-          <Reciptbutton
-            header="Slet automatik Kvitteringer efter 2 år"
-            onClickEvent={skiftAutomatiskSletning}
-            text={automatiskSletning}>
-          </Reciptbutton>
-          <Reciptbutton
-            header="Vis gamle kvitteringer"
-            onClickEvent={skiftVisGamleKvitteringer} 
-            text={visGamleKvitteringer}>
-          </Reciptbutton>
+          <div className="flex p-4 flex-col border-solid border-4  rounded-lg border-blue-400 ">
+            <h2>Slet automatik Kvitteringer efter 2 år </h2>
+            <button
+              onClick={skiftAutomatiskSletning}
+              className=" mt-2 border border-spacing-1 rounded-xl font-bold text-xl"
+            >
+              {automatiskSletning ? "Deaktivere" : "Aktivere"}
+            </button>
+          </div>
+           <div className="flex p-4 flex-col border-solid border-4  rounded-lg border-blue-400 ">
+            <h2>Vis gamle kvitteringer </h2>
+            <button
+              onClick={skiftVisGamleKvitteringer}
+              className=" mt-2 border border-spacing-1 rounded-xl font-bold text-xl"
+            >
+              {visGamleKvitteringer ? "Deaktivere" : "Aktivere"}
+            </button>
+          </div>
         </div>
       </div>
     </ProtectedRoute>
